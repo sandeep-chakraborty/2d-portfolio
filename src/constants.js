127fa8448dc095bbd959a4588aad04b96c042ad1
Sandeep Chakraborty <105798887+sandeep-chakraborty@users.noreply.github.com>
2024-04-11 12:48:38 +0530

    Adding v1 files

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+export const scaleFactor=4;
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript these days.
+    I've made some websites in that language. I also like Java and C. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/sandeep-chakraborty" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I'm still to receive it in 2025, it is not really teaching me much but i guess we will have to live with that!`,
+  "sofa-table": `That's my sofa. I like to relax here and read books or watch YouTube. 
+  I'm also active on Discord and twitter <a href="https://twitter.com/isitsandeep/" target="_blank">my Twitter</a>!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@chaiaurcode" target="_blank">Chai aur code</a>, <a href="https://www.youtube.com/@harkirat1" target="_blank">Harkirat singh</a>,
+  <a href="https://www.youtube.com/@Mrwhosetheboss" target="_blank">Mrwhosetheboss</a> and <a href="https://www.youtube.com/@casey" target="_blank">Casey!</a> (sometimes)!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it, I also like to read books right before i sleep`,
+  resume: `I'm still working on it <a href="about:blank" target="_blank">Will be uploaded later!</a>
+  Contact me at <a href="mailto:heyitssandeep@gmail.com">My email!</a> if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas, it's a great library to learn new things and it fascinated me alot`,
+  library: `There are a lot of books on my shelves, they are mostly classic litterature, novels and programming books, i even have one japanese manga (i can't read Japanese BTW).`,
+  exit: `Already done with my portfolio? If you wish to exit then please close the tab:)`,
+};
